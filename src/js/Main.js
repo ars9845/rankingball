d0a8889a7c5dfdad4d0800106f52dd50da96be81
kurt jeon <ars9845@naver.com>
2020-07-23 18:22:40 +0900

    ranking

--- src/js/Main.js
+++ src/js/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import * as utils from './common/utils';
+import Moment from "react-moment";
 import TopMenu from "./common/TopMenu";
 import MatchList from "./MatchList";
 import "../css/main.css";
@@ -8,12 +9,30 @@ import "../css/main.css";
 class Main extends React.Component {
   state = {
     isLoading: true,
-    matchs: []    
+    matchs: []
   };
   getMatch = async () =>{
-    const  {data : {result: matchs }} = await axios.get(utils.DATA_URL+"/game/106001/103001/105001?type=code");    
-    this.setState({matchs, isLoading :false}) ;    
-    console.log(matchs);        
+    const {data : {result: matchslist }} = await axios.get(utils.DATA_URL+"/game/106001/103001/105001?type=code");            
+    
+    const groupBy = (array, key) => {
+      return array.reduce((result, currentValue) => {                
+        var t = new Date(currentValue.st);                                
+        var formatted3 = t.toLocaleDateString("en-US");                 
+        var d = formatted3.split("/");         
+        var y = d.splice(-1)[0];        
+        d.splice(0, 0, y);        
+        var groupDate = d.join("");        
+        (result[groupDate] = result[groupDate] || []).push(
+          currentValue                    
+        );        
+        return result;
+      }, {});
+    };     
+    const matchs = groupBy(matchslist, "st");    
+    this.setState({matchs, isLoading :false});    
+    
+    console.log(matchs);     
+
   }; 
   componentDidMount(){
     this.getMatch();
@@ -22,7 +41,24 @@ class Main extends React.Component {
     this.setState({isLoading :true});
     //console.log(gametype);
     //console.log(submenu);
-    const  {data : {result: matchs }} = await axios.get(utils.DATA_URL+"game/"+`${submenu}`+"/103001/"+`${gametype}`+"?type=code");    
+    const {data : {result: matchslist }} = await axios.get(utils.DATA_URL+"game/"+`${submenu}`+"/103001/"+`${gametype}`+"?type=code");    
+    const groupBy = (array, key) => {
+      return array.reduce((result, currentValue) => {                
+        var t = new Date(currentValue.st);                                
+        var formatted3 = t.toLocaleDateString("en-US");                 
+        var d = formatted3.split("/");         
+        var y = d.splice(-1)[0];        
+        d.splice(0, 0, y);        
+        var groupDate = d.join("");        
+        (result[groupDate] = result[groupDate] || []).push(
+          currentValue                    
+        );        
+        return result;
+      }, {});
+    };     
+    const matchs = groupBy(matchslist, "st");    
+    this.setState({matchs, isLoading :false});    
+
     this.setState({matchs, isLoading :false});    
     console.log(matchs);  
   };  
@@ -31,9 +67,7 @@ class Main extends React.Component {
       var local_gametype = localStorage.getItem('gametype');      
       if(local_gametype === undefined){
           localStorage.setItem('gametype',105001);    
-      }
-
-      console.log(local_gametype);
+      }                 
     return (      
         <div className="content">
           <TopMenu onChange={this.getMatchChange} />
@@ -43,27 +77,58 @@ class Main extends React.Component {
             </div>
           ) : (
             <div>                
-              <ul className="match-list">  
-              {matchs.length || 0 ? (
+              <ul className="match-list">
+              
+              {Object.keys(matchs).map((key) => {
+                  return (
+                    <li key={key}>                    
+                        <h1>{key}</h1>                                                
+                        {matchs[key].map((match) => {
+                          return (                            
+                            <MatchList                        
+                              key={match.gi}
+                              id={match.gi}   
+                              gameid={match.gi} 
+                              gametype={match.gt} 
+                              homeabbr={match.hta} 
+                              awayabbr={match.ata}
+                              hometeam={match.htn} 
+                              awayteam={match.atn} 
+                              gp={match.gp} 
+                              cc={match.cc}              
+                              ln={match.ln}
+                              hs={match.hs}
+                              as={match.as}
+                              st={match.st}                      
+                            />                             
+                          )
+                        })}                                        
+                    </li>
+                  )
+                })}
+              
+              {/* {matchs.length || 0 ? (
                   matchs.map(match =>(                    
-                    <MatchList                     
+                    <MatchList                        
                       key={match.gi}
                       id={match.gi}   
                       gameid={match.gi} 
                       gametype={match.gt} 
-                      hometeam={match.hta} 
-                      awayteam={match.ata} 
+                      homeabbr={match.hta} 
+                      awayabbr={match.ata}
+                      hometeam={match.htn} 
+                      awayteam={match.atn} 
                       gp={match.gp} 
                       cc={match.cc}              
                       ln={match.ln}
                       hs={match.hs}
                       as={match.as}
-                      st={match.st}
+                      st={match.st}                      
                     />                  
                   ))                  
               ):(
                 <div className="no-list"><div className="txt1">no data</div></div>
-              )}                
+              )}                 */}
               </ul>
             </div>            
           )}          
--- src/js/MatchList.js
+++ src/js/MatchList.js
@@ -5,9 +5,8 @@ import * as utils from './common/utils';
 import Moment from "react-moment";
 import Countdown,{zeroPad} from 'react-countdown';
 
-function MatchList({ id, gameid, gametype, hometeam, awayteam, gp, cc, ln, hs, as, st}){    
-  return (
-    <li>
+function MatchList({ id, gameid, gametype,homeabbr, awayabbr, hometeam, awayteam, gp, cc, ln, hs, as, st}){          
+  return (    
       <div className={"match-box " +utils.getAbbr(gametype)}>
         <div className="match-top">
           <div className="league-box">
@@ -30,17 +29,43 @@ function MatchList({ id, gameid, gametype, hometeam, awayteam, gp, cc, ln, hs, a
               <span className="btn-stats off">Game Stats</span>
           </div>					
         </div> 
-        <div className="match-bottom"> 
-          <div className="team-box">
-            <div>
-              <span className="t-logo logo-LOL-RW"></span>							
-              <span className="t-team home">{hometeam} <em>&#40;{hs}&#41;</em></span>							
+        <div className="match-bottom">           
+          {gametype===104001 && (//soccer
+            <div className="soccer-team-box">
+              <div className="home">
+                <span className="t-team home">{hometeam} </span>
+                <span className="score"><em>&#40;{hs}&#41;</em></span>
+              </div>
+              <div className="away">
+                <span className="t-team away">{awayteam} </span>
+                <span className="score"><em>&#40;{as}&#41;</em></span>
+              </div>
             </div>
-            <div>
-              <span className="t-logo logo-LOL-ES"></span>
-              <span className="t-team away">{awayteam} <em>&#40;{as}&#41;</em></span>							
+          )}
+          {gametype===104002 || gametype===104012 && (//kbo,mlb
+            <div className="team-box">
+              <div>
+                <span className="t-logo logo-LOL-RW"></span>             
+                <span className="t-team home">{awayteam} <em>&#40;{as}&#41;</em></span>							
+              </div>
+              <div>
+                <span className="t-logo logo-LOL-ES"></span>
+                <span className="t-team away">{hometeam} <em>&#40;{hs}&#41;</em></span>							
+              </div>
             </div>
-          </div>
+          )}
+          {gametype===104003 || gametype===104004 || gametype===105001 && (//lol, nfl, nba 
+            <div className="team-box">
+              <div>
+                <span className="t-logo logo-LOL-RW"></span>             
+                <span className="t-team home">{awayabbr} <em>&#40;{as}&#41;</em></span>							
+              </div>
+              <div>
+                <span className="t-logo logo-LOL-ES"></span>
+                <span className="t-team away">{homeabbr} <em>&#40;{hs}&#41;</em></span>							
+              </div>
+            </div>
+          )}
           <div className="total-box">
             <p className="t-prize">GUARANTEED PRIZE</p>
             <div className="t-sum">            
@@ -48,11 +73,10 @@ function MatchList({ id, gameid, gametype, hometeam, awayteam, gp, cc, ln, hs, a
                 gp || undefined ? (gp.toLocaleString()):(0)              
               }</span>			
             </div>
-            <p className="t-total">TOTAL <span>13</span> CONTESTS</p>
+            <p className="t-total">TOTAL <span>{cc}</span> CONTESTS</p>
           </div>
         </div>
-      </div>          
-    </li>   
+      </div>             
   )
 };  
 export default MatchList;
