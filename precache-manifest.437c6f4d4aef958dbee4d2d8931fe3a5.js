66ac011d6f3ab5e9f0fec9286baf0b1ece9713ad
kurt jeon <ars9845@naver.com>
2020-08-26 18:01:23 +0900

    Updates

---
+++ precache-manifest.437c6f4d4aef958dbee4d2d8931fe3a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca16d2b5974f62e34a67e61588ad17d8",
+    "url": "/rankingball/index.html"
+  },
+  {
+    "revision": "2540212b4d784e47b290",
+    "url": "/rankingball/static/css/main.81cc27ae.chunk.css"
+  },
+  {
+    "revision": "469d00a61d1afc395c11",
+    "url": "/rankingball/static/js/2.c086601b.chunk.js"
+  },
+  {
+    "revision": "7e6ec0d9861ce35e695c4f042343a82f",
+    "url": "/rankingball/static/js/2.c086601b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2540212b4d784e47b290",
+    "url": "/rankingball/static/js/main.aeb367e8.chunk.js"
+  },
+  {
+    "revision": "9ef3acea05e1274eaa32",
+    "url": "/rankingball/static/js/runtime-main.d584256b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rankingball/precache-manifest.4423f02ddb0ec51dd2cc0be940865384.js"
+  "/rankingball/precache-manifest.437c6f4d4aef958dbee4d2d8931fe3a5.js"
 );
 
 self.addEventListener('message', (event) => {
